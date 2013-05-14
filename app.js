548deb8b4b2c96e5d0d4d8edd184ba10b0305ecb
Ningxia Zhang <thmmrs2298@gmail.com>
2013-05-14 15:54:27 -0700

    send report debug

--- app.js
+++ app.js
@@ -27,10 +27,9 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+app.all('/', function(req, res){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
 });
 
 app.get('/', routes.index);
